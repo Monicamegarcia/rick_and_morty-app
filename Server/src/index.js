b9e9f95c0993ce7801f920a494c5e0221eb76ca6
Monicamegarcia <monicamegarcia@gmail.com>
2023-06-20 11:58:16 -0300

    sequelize 1

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,10 +1,16 @@
 const {server} = require ("./app");
+const { conn } = require('./DB_connection');
 
 const PORT = 3001;
 
-server.listen (PORT, ()=> {
-    console.log (`server raised in: ${PORT}`);
-});
+conn.sync({force: true}).then(()=>{
+    server.listen (PORT, ()=> 
+    console.log (`server raised in: ${PORT}`))
+})
+
+//conn es database
+
+//conn.sync es una promesa, se llama al servidor que hay dos formas de hacerlo
 
 //se modulariza con app por los test
 //no puedo tener el listen en el mismo archivo que estoy testeando,
