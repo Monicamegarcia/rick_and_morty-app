6e034c53654e437daf704f1855eaddef819d73e2
Monicamegarcia <monicamegarcia@gmail.com>
2023-06-20 17:01:39 -0300

    sequelize 2

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,15 +1,15 @@
-const user= require("../utils/users");
+const { User} = requiere("../DB_connection.js");
 
-const login =(req, res) => {
-    const { email, password} = req.query;
-    const usersFound= users.find((user)=>user.email === email && user.password === password)
-
-    return usersFound
-    ? res.status(200).json({access: true})
-    : res.status(404).json ({access: false})
-}
-
-
-module.exports = {
-    login
+module.exports = async (req, res) => {
+    try {
+        const { email, password} = req.query;
+        if (!email || !password) return res.status (400). send("Faltan datos");
+        const user = await User.findOne({where:{email}})
+        if(!user) return res.status(404).send("Usuario no econtrado");
+        return user.password ===password
+        ?res.json ({access:true})
+        : res.status(403).send("Contraseña incorrecta");
+    } catch (error) {
+        return res.status(500).send (error.message);
+    }
 };
--- Server/src/utils/users.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{email: "monicamegarcia@gmail.com", password: "Moni"}];
